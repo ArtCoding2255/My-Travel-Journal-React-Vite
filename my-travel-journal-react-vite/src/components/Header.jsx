f34c80c56a8bdc2582b1559a6fb9ef834df31510
Pear <artcoding2544@gmail.com>
2025-04-24 05:57:04 +0700

    build header

--- my-travel-journal-react-vite/src/components/Header.jsx
+++ my-travel-journal-react-vite/src/components/Header.jsx
@@ -1,5 +1,10 @@
+import { FaGlobeAmericas } from "react-icons/fa";
+
 export default function Header(){
     return (
-        <h1>This is Header</h1>
+        <header>
+            <FaGlobeAmericas size={24} color="white" className='globe-icon'/>
+            <h1 className="header-text">My Travel Journal</h1>
+        </header>        
     )
 }
